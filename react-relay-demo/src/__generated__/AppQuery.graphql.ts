0caeb2944493356f5ebbb770fd70fd99dfc5a9e3
rigrergl <rigrergl@gmail.com>
2023-10-07 22:31:21 -0500

    setup relay

---
+++ react-relay-demo/src/__generated__/AppQuery.graphql.ts
@@ -0,0 +1,94 @@
+/**
+ * @generated SignedSource<<c6644b19931da44a30ff42c068eb70c6>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type AppQuery$variables = {};
+export type AppQuery$data = {
+  readonly viewer: {
+    readonly login: string;
+  };
+};
+export type AppQuery = {
+  response: AppQuery$data;
+  variables: AppQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "login",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "viewer",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "viewer",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "472b874baef51088a8d7ed0d9db65597",
+    "id": null,
+    "metadata": {},
+    "name": "AppQuery",
+    "operationKind": "query",
+    "text": "query AppQuery {\n  viewer {\n    login\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "de789ef98f1cd9ec113ca0f5d33201a1";
+
+export default node;
