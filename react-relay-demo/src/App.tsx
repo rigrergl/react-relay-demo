aa91ab93feb15e2f66bfb58f6e9074174c1da8a8
rigrergl <rigrergl@gmail.com>
2023-10-07 23:06:37 -0500

    setup navigation

--- react-relay-demo/src/App.tsx
+++ react-relay-demo/src/App.tsx
@@ -1,19 +1,57 @@
-import { graphql, useLazyLoadQuery } from "react-relay";
-import type { AppQuery } from "./__generated__/AppQuery.graphql";
-import { Typography } from "@mui/material";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { useState } from "react";
+import AccountPage from "./pages/AccountPage";
+import IssuesPage from "./pages/IssuesPage";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`wrapped-tabpanel-${index}`}
+      aria-labelledby={`wrapped-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function App() {
-  const AppQuery = graphql`
-    query AppQuery {
-      viewer {
-        login
-      }
-    }
-  `;
+  const [value, setValue] = useState(0);
 
-  const data = useLazyLoadQuery<AppQuery>(AppQuery, {});
+  const handleChange = (_event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
-  return <Typography>{data.viewer?.login}</Typography>;
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="app tabs" centered>
+          <Tab label="Issues" />
+          <Tab label="Account" />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <IssuesPage />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <AccountPage />
+      </TabPanel>
+    </Box>
+  );
 }
 
 export default App;
